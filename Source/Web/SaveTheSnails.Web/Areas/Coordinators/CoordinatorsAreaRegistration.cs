b3751c49b912a465b57b3675e2d7e1e1a495e8ec
dob-sal <dob_sal@yahoo.com>
2014-11-14 17:06:10 +0200

    Add coordinator and user areas. Install Glimpse.

---
+++ Source/Web/SaveTheSnails.Web/Areas/Coordinators/CoordinatorsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SaveTheSnails.Web.Areas.Coordinators
+{
+    public class CoordinatorsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Coordinators";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Coordinators_default",
+                "Coordinators/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
